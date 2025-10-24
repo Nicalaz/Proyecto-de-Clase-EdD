c6d39639103e9d779825921221e559eb6d327fd7
Nicolas Arciniegas <arciniegas.nicolas17@gmail.com>
2025-10-24 15:31:48 -0500

    actualizacion segunda entrega

---
+++ Segunda entrega.py
@@ -0,0 +1,13 @@
+#Segunda entrega
+class Nodo:
+    def __init__(self, nombre, distancia_acumulada):
+        self.nombre = nombre
+        self.distancia_acumulada = distancia_acumulada
+        self.siguiente = None
+
+#Clase Arbol
+
+class Arbol:
+    def __init__(self):
+        return
+    
